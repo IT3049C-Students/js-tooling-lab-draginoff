e28dd5dad86e6f46b8e0ed09411097dc2f86d011
draginoff <93340583+draginoff@users.noreply.github.com>
2022-07-03 16:06:14 -0400

    Completed Pages 3 & 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require(`path`);
 
 module.exports = {
-  entry: `./js-tooling-lab-draginoff/resources/scripts/index.js`,
+  entry: `./src/resources/scripts/index.js`,
   output: {
     filename: `main.js`,
     path: path.resolve(__dirname, `dist`),
